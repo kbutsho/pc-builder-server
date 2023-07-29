49e3ca5329f6d4f473081d4a40e4aba029606725
Kaushik Biswas <kbutsho@gmail.com>
2023-07-29 21:19:21 +0600

    added

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
+const cors = require('cors')
+const middleware = jsonServer.defaults();
+
+server.use(cors({ origin: '*' }));
+server.use(middleware);
+server.use(
+	jsonServer.rewriter({
+		"/api/*": "/$1",
+	})
+);
+server.use(router);
+server.listen(8000, () => {
+	console.log("server is running");
+});
+
+module.exports = server;
