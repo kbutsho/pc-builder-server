32fca633e94a9aa34d9c00d763904759521c8ea1
Kaushik Biswas <kbutsho@gmail.com>
2023-07-29 21:26:05 +0600

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router("db.json");
 const cors = require('cors')
 const middleware = jsonServer.defaults();
 
